3d15555cc36b5709c5aea3c32760777164a75cd6
sungmin <lmsgsm1@gmail.com>
2021-04-17 01:41:43 +0900

    add chapter3 code(3.5)

--- src/test/java/com/nesty/boot/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/nesty/boot/springboot/domain/posts/PostsRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,5 +42,20 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록(){
+        //given
+        LocalDateTime now = LocalDateTime.of(2021, 4, 17, 0, 0, 0);
+        postsRepository.save(Posts.builder().author("jenny").title("test title").content("text").build());
+        //when
+        List<Posts> all = postsRepository.findAll();
+
+        //then
+        Posts posts = all.get(0);
+        System.out.println(">>>>>>>>>> createDate = "+ posts.getCreatedDate()+", modifiedDate = "+posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
